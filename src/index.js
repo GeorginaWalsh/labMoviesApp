4cc2e4702d3696f8a5bf02bd533370b368d898e7
georginawalsh <20093147>
2022-10-28 16:16:02 +0100

    Add new review support.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 
+import AddMovieReviewPage from './pages/addMovieReviewPage'
+
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import {Link} from 'react-router-dom'
 
@@ -32,15 +34,17 @@ const App = () => {
       <BrowserRouter>
         <SiteHeader />
         <MoviesContextProvider>
-            <Routes>
-          <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
-          <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
+          <Routes>
+            <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+            <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
 
-          <Route exact path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+            <Route exact path="/movies/upcoming" element={<UpcomingMoviesPage />} />
 
-          <Route path="/movies/:id" element={<MoviePage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={ <Navigate to="/" /> } />
+            <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
+
+            <Route path="/movies/:id" element={<MoviePage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={ <Navigate to="/" /> } />
           </Routes>
         </MoviesContextProvider>
       </BrowserRouter>
